50a9ee10026c9a4eed9417ab63ef8ed2ec091344
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2022-01-06 15:38:19 +0000

    Started developing action

---
+++ install-spack.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+CURWD=$PWD
+
+echo "::group::Install FAIR-CLI"
+echo "::notice title=Data Store::Installing Spack to: ${SPACK_SRC_DIR}"
+git clone -c feature.manyFiles=true https://github.com/spack/spack.git ${SPACK_SRC_DIR}
+
+cd ${SPACK_SRC_DIR}
+
+if [ "${SPACK_REF}" == "latest" ]; then
+    INPUT_REF=$(git describe --tags --abbrev=0)
+fi
+
+echo "::notice title=Data Store::Checking out version: ${SPACK_REF}"
+git checkout ${SPACK_REF}
+
+cd ${CURWD}
+echo "::endgroup::"
